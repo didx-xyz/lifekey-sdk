457dace65b248ac9b8f4cb2dc8b4be35d7140518
Ant Cosentino <antony@mycosentino.com>
2017-03-15 16:23:03 +0200

    restructure the exports

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 
 'use strict'
 
-var lifekey = require('./lib/lifekey')()
-var agent = require('./lib/agent')
-
-agent.on('open', function(express, socket) {
-  console.log('listening on', socket.address())
-  agent.close()
-}).on('close', function() {
-  console.log('server closed')
-}).open()
+module.exports = {
+  lifekey: require('./lib/lifekey'),
+  agent: require('./lib/agent')
+}
