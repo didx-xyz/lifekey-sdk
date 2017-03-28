6055a5a9b6b79547ee562b870199eda418f90bbd
Ant Cosentino <antony@mycosentino.com>
2017-03-28 12:49:21 +0200

    adds functions for creating verifiable claims updates existing functions with the notion of is_verifiable_claim adds doc comments to all functions in lifekey server api client wrapper

--- src/lifekey.js
+++ src/lifekey.js
@@ -15,6 +15,16 @@ function sign(plain, private_key) {
   )
 }
 
+function verify(plain, signature, public_key) {
+  return public_key.hashAndVerify(
+    'sha256',
+    Buffer(plain),
+    signature,
+    'base64',
+    false
+  )
+}
+
 function request(method, path, headers, body, on_send) {
   var h = {'content-type': 'application/json'}
   if (headers) {
@@ -61,6 +71,11 @@ function parse_res(res, on_parsed) {
 module.exports = function(env) {
   return {
     user: {
+      /**
+       * update webhook url for your service
+       * @param webhook_url string
+       * @param on_update function
+       */
       update_webhook_uri: function(webhook_url, on_update) {
         if (!webhook_url) {
           return on_update(new Error('missing required arguments'))
@@ -76,6 +91,11 @@ module.exports = function(env) {
     },
     user_connection: {
       request: {
+        /**
+         * request connection with specified user
+         * @param user_id mixed
+         * @param on_send function
+         */
         send: function(user_id, on_send) {
           if (!user_id) {
             return on_send(new Error('missing required arguments'))
@@ -88,6 +108,12 @@ module.exports = function(env) {
             on_send
           )
         },
+        /**
+         * respond to specified connection request with given response value
+         * @param ucr_id mixed
+         * @param accepted boolean
+         * @param on_respond function
+         */
         respond: function(ucr_id, accepted, on_respond) {
           if (!(ucr_id && typeof accepted === 'boolean')) {
             return on_respond(new Error('missing required arguments'))
@@ -101,6 +127,10 @@ module.exports = function(env) {
           )
         }
       },
+      /**
+       * enumerate all active unacknowledged connections
+       * @param on_get function
+       */
       get_all: function(on_get) {
         request(
           'get',
@@ -110,6 +140,11 @@ module.exports = function(env) {
           on_get
         )
       },
+      /**
+       * detail a specified connection
+       * @param uc_id mixed
+       * @param on_get function
+       */
       get_one: function(uc_id, on_get) {
         if (!uc_id) {
           return on_get(new Error('missing required arguments'))
@@ -122,6 +157,11 @@ module.exports = function(env) {
           on_get
         )
       },
+      /**
+       * delete specified connection
+       * @param uc_id mixed
+       * @param on_delete function
+       */
       delete: function(uc_id, on_delete) {
         if (!uc_id) {
           return on_delete(new Error('missing required arguments'))
@@ -137,6 +177,11 @@ module.exports = function(env) {
     },
     information_sharing_agreement: {
       request: {
+        /**
+         * request isa with specified user
+         * @param isa object
+         * @param on_send function
+         */
         send: function(isa, on_send) {
           if (!(isa.user_id &&
                 isa.license &&
@@ -159,6 +204,12 @@ module.exports = function(env) {
             on_send
           )
         },
+        /**
+         * respond to specified isa request with given response value
+         * @param isar_id mixed
+         * @param response object
+         * @param on_respond function
+         */
         respond: function(isar_id, response, on_respond) {
           if (!(isar_id &&
                 typeof response.accepted === 'boolean' &&
@@ -174,6 +225,10 @@ module.exports = function(env) {
           )
         }
       },
+      /**
+       * enumerate all active and unacknowledged isas
+       * @param on_get function
+       */
       get_all: function(on_get) {
         request(
           'get',
@@ -183,6 +238,11 @@ module.exports = function(env) {
           on_get
         )
       },
+      /**
+       * detail a specified isa
+       * @param isa_id mixed
+       * @param on_get function
+       */
       get_one: function(isa_id, on_get) {
         request(
           'get',
@@ -192,6 +252,12 @@ module.exports = function(env) {
           on_get
         )
       },
+      /**
+       * update the original response to the isa request
+       * @param isa_id mixed
+       * @param permitted_resources array
+       * @param on_update function
+       */
       update: function(isa_id, permitted_resources, on_update) {
         request(
           'put',
@@ -201,6 +267,11 @@ module.exports = function(env) {
           on_update
         )
       },
+      /**
+       * delete the specified isa
+       * @param isa_id mixed
+       * @param on_delete function
+       */
       delete: function(isa_id, on_delete) {
         request(
           'delete',
@@ -210,6 +281,11 @@ module.exports = function(env) {
           on_delete
         )
       },
+      /**
+       * retrieve any resources agreed to be shared in specfied isa
+       * @param isa_id mixed
+       * @param on_pull function
+       */
       pull: function(isa_id, on_pull) {
         request(
           'get',
@@ -219,6 +295,12 @@ module.exports = function(env) {
           on_pull
         )
       },
+      /**
+       * send new resources in relation to specified isa
+       * @param isa_id mixed
+       * @param resources array
+       * @param on_push function
+       */
       push: function(isa_id, resources, on_push) {
         request(
           'post',
@@ -230,6 +312,90 @@ module.exports = function(env) {
       }
     },
     resource: {
+      verifiable_claim: {
+        /**
+         * create a verifiable claim with given resource parameters signed with your private key
+         * @param resource object
+         * @param on_create function
+         */
+        create: function(resource, on_create) {
+          // resource.context array (one or more urls pointing to jsonld definitions)
+          // resource.is_credential boolean (will this verfiable claim be shown as a badge in the app?)
+          // resource.issued_for mixed (user id or did for whom this claim is issued)
+          // resource.creator mixed (you)
+          // resource.additional_fields object (gets included in the "claim" object such that each field is merged directly - each additional field must map to a field in your jsonld definition (which is referenced in the @context array))
+          if (!(resource.context &&
+                resource.is_credential &&
+                resource.issued_for &&
+                resource.created_at &&
+                resource.additional_fields)) {
+            return on_create(
+              new Error(
+                'missing any of the following claim parameters: ' +
+                'context, issued_for, creator, created_at, ' +
+                'additional_fields'
+              )
+            )
+          }
+
+          var claim_instance = {
+            '@context': [
+              'http://schema.cnsnt.io/verifiable_claim'
+            ].concat(resource.context),
+            claim: {
+              isCredential: resource.is_credential,
+              issuedFor: resource.issued_for,
+              creator: env.USER.ID,
+              createdAt: resource.created_at
+            },
+            signatureValue: ''
+          }
+
+          Object.keys(additional_fields).forEach(function(field) {
+            claim_instance.claim[field] = additional_fields[field]
+          })
+
+          claim_instance.signatureValue = sign(
+            JSON.stringify(claim_instance.claim),
+            env.USER.PRIVATE_KEY
+          )
+
+          return on_create(null, claim_instance)
+        },
+        /**
+         * ensure that the enclosed signature in the given verifiable claim was created by the user who owns the given public key
+         * @param verifiable_claim object
+         * @param public_key string
+         * @param on_verify function
+         */
+        verify: function(verifiable_claim, public_key, on_verify) {
+
+          if (!('claim' in verifiable_claim &&
+                'signatureValue' in verifiable_claim)) {
+            return on_verify(new Error('given claim value is not verifiable'))
+          }
+          
+          var plaintext = JSON.stringify(verifiable_claim.claim)
+          var signature = verifiable_claim.signatureValue
+          
+          try {
+            var ursa_public_key = ursa.coercePublicKey(public_key)
+          } catch (e) {
+            return on_verify(
+              new Error(
+                'unable to initialise ursa public key instance with given public key value'
+              )
+            )
+          }
+
+          return on_verify(null, verify(plaintext, signature, ursa_public_key))
+        }
+      },
+      /**
+       * request index of all resources (or optionally any resources that were pushed by other users)
+       * @param get_pushed boolean
+       * @param on_index function
+       */
       index: function(get_pushed, on_index) {
         request(
           'get',
@@ -239,6 +405,11 @@ module.exports = function(env) {
           on_index
         )
       },
+      /**
+       * create the given resource
+       * @param resource object
+       * @param on_create function
+       */
       create: function(resource, on_create) {
         request(
           'post',
@@ -253,11 +424,17 @@ module.exports = function(env) {
             mime: resource.mime || 'text/plain',
             value: resource.value,
             is_default: resource.is_default || false,
+            is_verifiable_claim: resource.is_verifiable_claim || false,
             is_archived: resource.is_archived || false
           }),
           on_create
         )
       },
+      /**
+       * request the specified resource
+       * @param resource_id mixed
+       * @param on_get function
+       */
       get_one: function(resource_id, on_get) {
         request(
           'get',
@@ -267,6 +444,12 @@ module.exports = function(env) {
           on_get
         )
       },
+      /**
+       * update the resource specified by id with given resource update values
+       * @param resource_id mixed
+       * @param resource object
+       * @param on_update function
+       */
       update: function(resource_id, resource, on_update) {
         request(
           'put',
@@ -282,11 +465,17 @@ module.exports = function(env) {
             schema: resource.schema,
             value: resource.value,
             is_default: resource.is_default || false,
+            is_verifiable_claim: resource.is_verifiable_claim || false,
             is_archived: resource.is_archived || false
           }),
           on_update
         )
       },
+      /**
+       * delete the specified resource
+       * @param resource_id mixed
+       * @param on_delete function
+       */
       delete: function(resource_id, on_delete) {
         request(
           'delete',
@@ -294,6 +483,23 @@ module.exports = function(env) {
           auth_headers(env.USER, Date.now()),
           null,
           on_delete
+        )
+      }
+    },
+    key: {
+      /**
+       * request the aliased public key of specified user (omitting alias returns the user's default signing public key)
+       * @param user_id mixed
+       * @param alias string
+       * @param on_get
+       */
+      get: function(user_id, alias, on_get) {
+        request(
+          'get',
+          '/management/key/' + user_id + (alias ? '?alias=' + alias : ''),
+          auth_headers(env.USER, Date.now()),
+          null,
+          on_get
         )
       }
     }
